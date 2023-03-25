8e1d668904190c7f9f838b658aff2eff926544cf
vlad <sp0ngbik@gmail.com>
2023-03-25 15:22:04 +0300

    get weather

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import { Route, Routes } from "react-router-dom";
-import WelcomePage from "./roadMap/WelcomePage";
-import WeatherInfo from "./WeatherInfo";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [nameOfCountry, setNameOfCountry] = useState();
+  const [currentTemp, setCurrentTemp] = useState();
+  const getCountyCords = () => {
+    const feelsLikeKelvin = 273.15;
+    const apiKey = "5eac858cbc638bf9cf41fef1f454746f";
+    const apiRequest = `http://api.openweathermap.org/geo/1.0/direct?q=${nameOfCountry}&appid=${apiKey}`;
+    axios
+      .get(apiRequest)
+      .then((dataCountry) => {
+        console.log(dataCountry);
+        return axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${dataCountry.data[0].lat}&lon=${dataCountry.data[0].lon}&appid=${apiKey}`
+        );
+      })
+      .then((ar) => {
+        setCurrentTemp(parseInt(ar.data.main.temp_max - feelsLikeKelvin));
+      })
+      .catch(() => {
+        alert("Sorry, but your country was not found");
+      });
+  };
   return (
-    <Routes>
-      <Route path="*" element={<WelcomePage />} />
-      <Route path="/weather_info" element={<WeatherInfo />} />
-    </Routes>
+    <div>
+      <div>hello</div>
+      <input
+        placeholder="chose your country"
+        onChange={(e) => {
+          setNameOfCountry(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          console.log(getCountyCords());
+        }}
+      >
+        sad
+      </button>
+    </div>
   );
 }
 
