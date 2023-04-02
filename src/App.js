71944af77628285afd5159781cb6fd70c873caf3
vlad <sp0ngbik@gmail.com>
2023-04-02 11:10:39 +0300

    before func

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import axios from "axios";
 import { useState } from "react";
 import style from "./style.module.scss";
 import searchImage from "./image/4708645.png";
+import { currentData, currentWeatherData } from "./functionsWeather";
 function App() {
   const [nameOfCountry, setNameOfCountry] = useState();
   const [weatherData, setWeatherData] = useState();
   const [currentTemp, setCurrentTemp] = useState();
   const [openInfoWeather, setopenInfoWeather] = useState();
+
   const getCountyCords = () => {
     const feelsLikeKelvin = 273.15;
     const apiKey = "5eac858cbc638bf9cf41fef1f454746f";
-    const apiRequest = `http://api.openweathermap.org/geo/1.0/direct?q=${nameOfCountry}&appid=${apiKey}`;
+    const apiRequest = `http://api.openweathermap.org/geo/1.0/direct?q=${nameOfCountry}&limit=5&appid=${apiKey}`;
     axios
       .get(apiRequest)
       .then((dataCountry) => {
         return axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${dataCountry.data[0].lat}&lon=${dataCountry.data[0].lon}&appid=${apiKey}`
+          `https://api.openweathermap.org/data/2.5/weather?lat=${dataCountry.data[0].lat}&limit=5&lon=${dataCountry.data[0].lon}&appid=${apiKey}`
         );
       })
       .then((weatherData) => {
@@ -29,6 +31,7 @@ function App() {
         alert("Sorry, but your country was not found");
       });
   };
+
   return (
     <div className={style.mainWeatherBlock}>
       <div className={style.weatherContent}>
@@ -49,24 +52,30 @@ function App() {
           </button>
         </div>
         <div className={style.weatherDataStyle}>
-          {weatherData && (
-            <div
-              className={`${style.temperatureStyle} ${
-                openInfoWeather ?? style.openInfo
-              }`}
-            >
-              <div>
-                <h3>Country: {weatherData.data.sys.country}</h3>
-                <span>{currentTemp && currentTemp} °C</span>
-                <img
-                  src={`https://openweathermap.org/img/wn/${weatherData.data.weather[0].icon}@2x.png`}
-                  alt="Sorry but your country not found"
-                />
-                {/* <span>{weatherData.data.weather[0].main}</span>
-                <span>{weatherData.data.weather[0].description}</span> */}
-              </div>
-            </div>
-          )}
+          {
+            weatherData && currentWeatherData(weatherData)
+            //   <div
+            //   className={`${style.temperatureStyle} ${
+            //     openInfoWeather ?? style.openInfo
+            //   }`}
+            //   >
+            //   <div className={style.mainInfo}>
+            //   <h3>Country: {weatherData.data.sys.country}</h3>
+            //   <div>
+            //       <span>{currentTemp && currentTemp} °C</span>
+            //       <img
+            //         src={`https://openweathermap.org/img/wn/${weatherData.data.weather[0].icon}@2x.png`}
+            //         alt="Sorry but your country not found"
+            //         />
+            //     </div>
+            //     <span>{weatherData.data.weather[0].main}</span>
+            //     <span>{weatherData.data.weather[0].description}</span>
+            //     </div>
+            //     <div>
+            //     {currentData(weatherData.data.timezone, weatherData.data.dt)}
+            //   </div>
+            // </div>
+          }
         </div>
       </div>
     </div>
---
+++ src/functionsWeather.js
@@ -0,0 +1,17 @@
+export const currentData = (timeZone, dt) => {
+  const date = new Date((dt + timeZone) * 1000);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours() - 2;
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const daysOfWeek = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
+  const dayOfWeek = daysOfWeek[date.getDay()];
+  const formattedDateTime = `${day}.${month}.${year}, ${hours}:${minutes}:${seconds}, ${dayOfWeek}`;
+  return formattedDateTime;
+};
+
+export const currentWeatherData = (countrysCounter) => {
+  for (let counter = 0; counter <= countrysCounter.length; counter++) {}
+};
